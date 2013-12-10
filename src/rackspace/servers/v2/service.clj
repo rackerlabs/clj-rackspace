dd897274a9e9111d0cbd3ad4cc76e47564a15e81
Sean Adams <sean.adams@rackspace.com>
2013-12-10 12:20:19 -0600

    Get new server payload

--- src/rackspace/servers/v2/service.clj
+++ src/rackspace/servers/v2/service.clj
@@ -1,3 +1,9 @@
 (ns rackspace.servers.v2.service
-  (:require [rackspace.const :as const]
-            [rackspace.services :as services]))
+  (:require [clojure.data.json :as json]))
+
+(defn get-new-server-payload [server-name image-id flavor-id]
+  {:body (json/write-str {:server
+                          {:name server-name
+                           :imageRef image-id
+                           :flavorRef flavor-id}})
+   :headers {"Content-Type" "application/json"}})
